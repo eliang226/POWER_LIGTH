44788247cc9f271e068c6b8730414888f142cd3a
eliang226 <naruto_g226@hotmail.com>
2026-02-19 11:59:22 -0400

    Initial commit of project files

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <PZEM004Tv30.h>
+
+#define NEO_PIXEL_PIN D3
+
+Adafruit_NeoPixel pixels(1, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+ 
+ 
+  pixels.begin();
+  pixels.setBrightness(50);
+  pixels.show();
+
+  
+
+}
+
+void loop() {
+
+}
+
